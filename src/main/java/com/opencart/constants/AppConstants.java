8a372d66a112e16d27cdffc3a71002e3ab335735
karthikrepo1289 <156223430+karthikrepo1289@users.noreply.github.com>
2024-05-08 23:17:11 +0530

    Update AppConstants.java

--- src/main/java/com/opencart/constants/AppConstants.java
+++ src/main/java/com/opencart/constants/AppConstants.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class AppConstants {
-
+	//Adding comment
 	public static final String registerAccount = "Register Account";
 	public static final List<String> accntPageFeatures = Arrays.asList("My Account", "Edit Account", "Password", 
 			"Address Book", "Wish List", "Order History", "Downloads", "Recurring payments", "Reward Points", "Returns", "Transactions", "Newsletter", "Logout");
